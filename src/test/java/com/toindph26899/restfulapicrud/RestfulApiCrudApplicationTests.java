9e7ff1b658d19d2d30ce59ddaad5c6090f89b364
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-05-05 13:47:55 +0700

    create project

---
+++ src/test/java/com/toindph26899/restfulapicrud/RestfulApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toindph26899.restfulapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
