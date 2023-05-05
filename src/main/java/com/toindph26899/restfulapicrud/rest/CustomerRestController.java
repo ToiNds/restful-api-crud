4de5450affdac307f41e746b0a9a6e90e5792151
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-05-05 14:55:12 +0700

    define crud restcontroller

--- src/main/java/com/toindph26899/restfulapicrud/rest/CustomerRestController.java
+++ src/main/java/com/toindph26899/restfulapicrud/rest/CustomerRestController.java
@@ -5,11 +5,13 @@ import com.toindph26899.restfulapicrud.request.CustomerRequest;
 import com.toindph26899.restfulapicrud.response.CustomerResponse;
 import com.toindph26899.restfulapicrud.service.CustomerService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,5 +34,15 @@ public class CustomerRestController {
     @PostMapping("/customers")
     public Customer save(@Valid @RequestBody CustomerRequest customerRequest) {
         return customerService.save(customerRequest);
+    }
+
+    @PutMapping("/customers")
+    public Customer update(@Valid @RequestBody CustomerRequest customerRequest) {
+        return customerService.save(customerRequest);
+    }
+
+    @DeleteMapping("/customers")
+    public void delete(@RequestParam("customerId") Long id) {
+        customerService.delete(id);
     }
 }
