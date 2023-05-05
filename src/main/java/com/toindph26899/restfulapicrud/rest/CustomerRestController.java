c6b042e025ee512d4f9b4f63148c1173eefde1ec
DESKTOP-A8MA4R6\os <toindph26899@fpt.edu.vn>
2023-05-05 14:51:45 +0700

    define save

---
+++ src/main/java/com/toindph26899/restfulapicrud/rest/CustomerRestController.java
@@ -0,0 +1,36 @@
+package com.toindph26899.restfulapicrud.rest;
+
+import com.toindph26899.restfulapicrud.entity.Customer;
+import com.toindph26899.restfulapicrud.request.CustomerRequest;
+import com.toindph26899.restfulapicrud.response.CustomerResponse;
+import com.toindph26899.restfulapicrud.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+    private CustomerService customerService;
+
+    public CustomerRestController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/customers")
+    public List<CustomerResponse> findAll() {
+        return customerService.findAll();
+    }
+
+    @PostMapping("/customers")
+    public Customer save(@Valid @RequestBody CustomerRequest customerRequest) {
+        return customerService.save(customerRequest);
+    }
+}
